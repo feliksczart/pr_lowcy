bf20296108f472b4087209b6ddabff284c4f0fbc
Student inf141205 <inf141205@polluks.cs.put.poznan.pl>
2021-05-12 12:47:30 +0200

    init

---
+++ Makefile
@@ -0,0 +1,18 @@
+SOURCES=$(wildcard *.cpp)
+HEADERS=$(SOURCES:.cpp=.h)
+FLAGS=-DDEBUG -g
+
+all: main
+
+main: $(SOURCES) $(HEADERS) 
+	mpicc $(SOURCES) $(FLAGS) -o main
+
+clear: 
+	clean
+
+clean: 
+	rm main a.out
+
+run: main 
+	mpirun -np 8 ./main
+
---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include "main.h"
+
+void check_thread_support(int provided){
+    printf("THREAD SUPPORT: chcemy %d. Co otrzymamy?\n", provided);
+    switch (provided) {
+        case MPI_THREAD_SINGLE: 
+            printf("Brak wsparcia dla wątków, kończę\n");
+            /* Nie ma co, trzeba wychodzić */
+	    fprintf(stderr, "Brak wystarczającego wsparcia dla wątków - wychodzę!\n");
+	    MPI_Finalize();
+	    exit(-1);
+	    break;
+        case MPI_THREAD_FUNNELED: 
+            printf("tylko te wątki, ktore wykonaly mpi_init_thread mogą wykonać wołania do biblioteki mpi\n");
+	    break;
+        case MPI_THREAD_SERIALIZED: 
+            /* Potrzebne zamki wokół wywołań biblioteki MPI */
+            printf("tylko jeden watek naraz może wykonać wołania do biblioteki MPI\n");
+	    break;
+        case MPI_THREAD_MULTIPLE: printf("Pełne wsparcie dla wątków\n"); /* tego chcemy. Wszystkie inne powodują problemy */
+	    break;
+        default: printf("Nikt nic nie wie\n");
+    }
+}
+
+void init(int *argc, char ***argv){
+	
+	int provided;
+	MPI_Init_thread(argc, argv,MPI_THREAD_MULTIPLE, &provided);
+	check_thread_support(provided);
+}
+
+int main(int argc, char **argv){
+	
+	init(&argc,&argv);
+}
