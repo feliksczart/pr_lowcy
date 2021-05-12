ac465d9e4990728803572e0573cd18e13b04682a
Student inf141205 <inf141205@polluks.cs.put.poznan.pl>
2021-05-12 13:16:07 +0200

    support rebuild

--- main.cpp
+++ main.cpp
@@ -1,24 +1,31 @@
 #include "main.h"
 
+bool thread_support = FALSE;
+
 bool check_thread_support(int provided) {
     return provided >= MPI_THREAD_MULTIPLE;
 }
 
-bool init(int *argc, char ***argv){
+void init(int *argc, char ***argv){
 	
 	int provided;
 	MPI_Init_thread(argc, argv,MPI_THREAD_MULTIPLE, &provided);
-	return check_thread_support(provided);
+	thread_support = check_thread_support(provided);
+}
+
+void finalize(){
+
+	MPI_Finalize();
 }
 
 int main(int argc, char **argv){
 	
-	bool thread_support = init(&argc,&argv);
+	init(&argc,&argv);
 	if(thread_support){
 		printf("Support Granted!\n");
 	} else{
 		printf("Support Not Granted!\n");
 	}
-	MPI_Finalize();
+	finalize();
 	return 0;
 }
