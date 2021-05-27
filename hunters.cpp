4200f9b1a58c506374283e1d5aaf573e702c1cc2
Student inf141205 <inf141205@polluks.cs.put.poznan.pl>
2021-05-27 18:12:34 +0200

    ?

--- hunters.cpp
+++ hunters.cpp
@@ -10,30 +10,22 @@ void Hunters::loop(int size, int rank){
 	pthread_create(&incomingMissionThread,NULL,&incomingMissionMonitor,NULL);
 	
 	while(1){
-		sleep(2);
 		pthread_mutex_lock(&Monitor::incomingMissionMutex);
 		pthread_mutex_lock(&Monitor::messageQMutex);
-		if(Hunters::state == HuntersState::WAITING_ORDER){
-			if(!Monitor::messageQ.empty()){
-				packet_t packet = Monitor::messageQ.front();
-				Monitor::messageQ.pop();
-				packet.lamport = Monitor::getLamport();
+		if(!Monitor::messageQ.empty()){
+			sleep(2);
+			packet_t packet = Monitor::messageQ.front();
+			Monitor::messageQ.pop();
+			packet.lamport = Monitor::getLamport();
+			if(Hunters::state == HuntersState::WAITING_ORDER){
 				sleep(rand()%15+1);
 				std::cout << GREEN << packet.lamport << ": Łowca " << rank << " otrzymał wiadomość o dostępnym zleceniu nr " << packet.orderNumber << RESET << std::endl;
-				Hunters::handleNewMessage(packet);
+			} else if(Hunters::state == HuntersState::TRYING_ORDER){
+				std::cout << GREEN << packet.lamport << ": Łowca " << rank << " otrzymał request o przydzielenie zlecenia " << packet.orderNumber << " dla łowcy " << packet.from << RESET << std::endl;	
 			}
-		} else if(Hunters::state == HuntersState::TRYING_ORDER){
-                        if(!Monitor::messageQ.empty()){
-                                packet_t packet = Monitor::messageQ.front();
-                                Monitor::messageQ.pop();
-                                packet.lamport = Monitor::getLamport();
-				sleep(rand()%15+1);
-                                std::cout << GREEN << packet.lamport << ": Łowca " << rank << " otrzymał request o przydzielenie zlecenia dla łowcy: " << packet.from << RESET << std::endl;
-                                Hunters::handleNewMessage(packet);
-                        }
-                }
-
-
+			Hunters::handleNewMessage(packet);
+		}
+                        
 		pthread_mutex_unlock(&Monitor::messageQMutex);
 		pthread_mutex_unlock(&Monitor::incomingMissionMutex);
 
@@ -63,9 +55,22 @@ void Hunters::handleNewMessage(packet_t packet){
 		//Monitor::print_map(Monitor::missions_queues);
 		Monitor::incrementLamport();
 		packet.lamport = Monitor::getLamport();
-		sleep(rand()%2+1);
+		//sleep(rand()%2+1);
 		std::cout << YELLOW << packet.lamport << ": Łowca " << Monitor::rank << " pyta innych czy może wyruszyć na misję"  << RESET << std::endl;
 		Hunters::sendOrderReq(packet);
+	} else if(packet.tag == ORDER_REQ){
+		auto firstInQueue = Monitor::missions_queues.find(packet.orderNumber);
+		if(packet.lamport > firstInQueue->second.at(0).first){
+			//send you cant
+		} else if(packet.lamport < firstInQueue->second.at(0).first){
+			//update kolejki
+			//wysłanie do tego co był, że nie może
+		} else if(packet.from > firstInQueue->second.at(0).first){
+			//send you cant
+		} else if(packet.from < firstInQueue->second.at(0).first){
+			//update kolejki
+                        //wysłanie do tego co był, że nie może
+		}
 	}
 }
 
